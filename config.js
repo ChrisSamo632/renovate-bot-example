d4d25fe32299b8704d4030c06615ca0f13b420bd
Chris Sampson <chris.sampson@naimuri.com>
2023-04-11 08:10:25 +0100

    feat(renovate): example renovate-bot setup for docker registry auth investigation

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    hostRules: [{
+        // Will be passed as `//gitlab.example/api/v4/projects/xxx/packages/npm/:_authToken=<TOKEN>` to `.npmrc`
+        hostType: 'npm',
+        matchHost: 'https://gitlab.example/api/v4/projects/xxx/packages/npm/',
+        token: process.env.RENOVATE_TOKEN || process.env.CI_JOB_TOKEN
+    },
+    {
+        hostType: 'docker',
+        matchHost: 'registry.gitlab.example',
+        username: process.env.CI_REGISTRY_USER,
+        password: process.env.CI_REGISTRY_PASSWORD
+    },
+    {
+        hostType: 'docker',
+        matchHost: 'gitlab.example:443',
+        username: process.env.CI_DEPENDENCY_PROXY_USER,
+        password: process.env.CI_DEPENDENCY_PROXY_PASSWORD
+    },
+    {
+        hostType: 'docker',
+        matchHost: 'gitlab.example',
+        username: process.env.CI_DEPENDENCY_PROXY_USER,
+        password: process.env.CI_DEPENDENCY_PROXY_PASSWORD
+    }
+    ]
+};
